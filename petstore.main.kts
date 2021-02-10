0c2d4206df555271d2c1b2d12c86d717032fbd65
Sebastian Lehrbaum <findusl@users.noreply.github.com>
2021-02-10 12:07:31 +0100

    Update petstore.main.kts
    Shorten

--- petstore.main.kts
+++ petstore.main.kts
@@ -1,41 +1,19 @@
 // run with kotlin petstore.main.kts
 
-@file:DependsOn("com.squareup.retrofit2:retrofit:+")
-@file:DependsOn("com.squareup.retrofit2:converter-gson:+")
-@file:DependsOn("com.squareup.retrofit2:adapter-rxjava2:+")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:+")
+@file:DependsOn("io.ktor:ktor-client-okhttp:+")
+@file:DependsOn("io.ktor:ktor-client-gson:+")
 
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import kotlin.system.exitProcess
 
-val BASE_URL = "https://petstore.swagger.io/v2/"
+data class InventoryResponse(val sold: Int, val pending: Int, val available: Int)
 
-val client = OkHttpClient.Builder().build()
-val retrofit = Retrofit.Builder()
-    .addConverterFactory(GsonConverterFactory.create())
-    .client(client)
-    .baseUrl(BASE_URL)
-    .build()
-val restApi = retrofit.create(RestAPI::class.java)
-
-runBlocking {
-    val inv = restApi.getInventory()
-    println("Result: Sold ${inv.sold}, pending ${inv.pending}, available ${inv.available}")
-}
-
-exitProcess(0)
-
-data class InventoryResponse (
-    val sold: Int,
-    val pending: Int,
-    val available: Int
-)
-
-interface RestAPI {
-    @GET("store/inventory")
-	suspend fun getInventory(): InventoryResponse
+val url = "https://petstore.swagger.io/v2/store/inventory"
+HttpClient { install(JsonFeature) }.use { client ->
+    runBlocking {
+        val (sold, pending, available) = client.get<InventoryResponse>(url)
+        println("Result: Sold $sold, pending $pending, available $available")
+    }
 }
